8fe1eeca86478b972d5bcf960d55162ef33434d6
marasame33 <250453229@qq.com>
2021-01-25 21:59:23 +0800

    update

--- shuati/src/leetcode/BinaryTree.java
+++ shuati/src/leetcode/BinaryTree.java
@@ -1,16 +1,44 @@
 package leetcode;
 
-import java.util.List;
+import Utils.ArrayUtils;
+import com.google.common.collect.Lists;
 
-public class  BinaryTree {
+import java.util.List;
+import java.util.Stack;
+
+public class BinaryTree {
 
     /**
      * 非递归后续遍历
      *
      * @param root
      */
-    public List<Integer> postorderTraversal(BinaryTreeNode root) {
-        return null;
+    public static List<Integer> postorderTraversal(BinaryTreeNode root) {
+        List<Integer> res = Lists.newArrayList();
+        BinaryTreeNode lastVisit = root;
+        Stack<BinaryTreeNode> stack = new Stack<>();
+        if (root == null) {
+            return Lists.newArrayList();
+        }
+        while(root!=null){
+            stack.push(root);
+            root = root.left;
+        }
+        while (!stack.isEmpty()) {
+            BinaryTreeNode p = stack.pop();
+            if (p.right == null || p.right == lastVisit) {
+                lastVisit = p;
+                res.add(p.val);
+            } else {
+                stack.push(p);
+                p = p.right;
+                while(p!=null){
+                    stack.push(p);
+                    p = p.left;
+                }
+            }
+        }
+        return res;
     }
 
     /**
@@ -34,40 +62,31 @@ public class  BinaryTree {
 
     static class BinaryTreeNode {
 
-        private int val;
+        public int val;
 
-        private BinaryTreeNode left;
+        public BinaryTreeNode left;
 
-        private BinaryTreeNode right;
+        public BinaryTreeNode right;
 
         public BinaryTreeNode(int val) {
             this.val = val;
             this.left = null;
             this.right = null;
         }
+    }
 
-        public int getVal() {
-            return val;
-        }
-
-        public void setVal(int val) {
-            this.val = val;
-        }
-
-        public BinaryTreeNode getLeft() {
-            return left;
-        }
-
-        public void setLeft(BinaryTreeNode left) {
-            this.left = left;
-        }
-
-        public BinaryTreeNode getRight() {
-            return right;
-        }
-
-        public void setRight(BinaryTreeNode right) {
-            this.right = right;
-        }
+    public static void main(String[] args) {
+        BinaryTreeNode root = new BinaryTreeNode(1);
+        BinaryTreeNode node1 = new BinaryTreeNode(2);
+        BinaryTreeNode node2 = new BinaryTreeNode(3);
+        BinaryTreeNode node3 = new BinaryTreeNode(4);
+        BinaryTreeNode node4 = new BinaryTreeNode(5);
+        BinaryTreeNode node5 = new BinaryTreeNode(6);
+        root.left = node1;
+        root.right = node2;
+        node1.left = node3;
+        node1.right = node4;
+        node2.right = node5;
+        postorderTraversal(root).forEach(System.out::println);
     }
 }
